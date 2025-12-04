8dc459177cfa484bc489764dd0041674104f7373
budkacik <60047286+budkacik@users.noreply.github.com>
2025-12-04 13:13:38 +0100

    update

---  1-10/5.py
+++  1-10/5.py
@@ -1,4 +1,12 @@
 from random import randint as rnd
 
-vals = [rnd(1, 1000) for x in range(150)]
-print(vals)
+vals = [rnd(1, 1000) for x in range(150)]  # Vygeneruje Zźoznam pozostávajuci zo 150 náhodných čísel od 1 po 1000
+
+first = 0
+second = None
+
+for val in vals:
+    if val > first:
+        second = first
+        first = val
+print(second)
