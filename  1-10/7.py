3838c5d87d864cf20f5c795b69d528fae8d50db2
budkacik <60047286+budkacik@users.noreply.github.com>
2025-12-04 14:50:50 +0100

    update

---
+++  1-10/7.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from random import randint as rnd
+from math import sin, cos, radians
+
+root = tk.Tk()
+c = tk.Canvas(root, width=500, height=500, bg="blue")
+c.pack()
+
+
+def create_star(cordx: int, cordy: int):
+    size = 15
+    for i in range(len(points)):
+        c.create_line(cordx - size * points[i][1], cordy - size * points[i][0], cordx - size * opposites[i][1], cordy - size * opposites[i][0], fill="yellow", width=1)
+        tempsize = size
+        size -= 2
+        c.create_line(cordx - size * points[i][1], cordy - size * points[i][0], cordx - size * opposites[i][1], cordy - size * opposites[i][0], fill="yellow", width=2)
+        size -= 3
+        c.create_line(cordx - size * points[i][1], cordy - size * points[i][0], cordx - size * opposites[i][1], cordy - size * opposites[i][0], fill="yellow", width=3)
+        c.create_oval(cordx - 3, cordy - 3, cordx + 3, cordy + 3, fill="yellow", outline="yellow")
+        size = tempsize
+
+
+global points, opposites
+points = [[sin(radians(x)), cos(radians(x))] for x in range(18, 307, 72)]
+opposites = points[3:] + points[:3]
+
+for i in range(50):
+    create_star(rnd(15, 485), rnd(15, 485))
+
+root.mainloop()
