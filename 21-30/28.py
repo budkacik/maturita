ca32fd5b95edb8f076bc92dba4ed46535c42158b
budkacik <60047286+budkacik@users.noreply.github.com>
2025-12-05 22:39:19 +0100

    update

--- 21-30/28.py
+++ 21-30/28.py
@@ -1,22 +1,21 @@
 file = open("28.vety.txt", "r", encoding="UTF-8")
 
 long = 0
+i = 1
 
 for line in file:
-    i = 1
     if len(line) > long:
-        lon = len(line)
+        long = len(line)
         target = i
     i += 1
 
 print(f"najdlhsia veta je na riadku {target}\n")
 
 file = open("28.vety.txt", "r", encoding="UTF-8")
-samohlasky = "AÁIÍYÝEÉUÚOÓaäiíyýeéuúoóô"
+samohlasky = "AÁIÍYÝEÉUÚOÓaáäiíyýeéuúoóô"
+
 for line in file:
     out = ""
     for char in line:
         out += char if char not in samohlasky else "*"
     print(out.strip())
-
-
